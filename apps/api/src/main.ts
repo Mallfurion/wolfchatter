63f8ef9358be17e896cf7379c3bc32345019c070
florin <florin.tomozei@spyhce.com>
2021-03-21 23:13:23 +0200

    add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,18 +3,27 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  // validation
+  app.useGlobalPipes(new ValidationPipe());
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Wolfchatter')
+    .setDescription('The wolfchatter API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port + '/');
   });
 }
 
