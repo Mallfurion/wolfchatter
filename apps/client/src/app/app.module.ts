f70e27f9447cad8a5e957d1b785442e7368c0dd5
florin <florin.tomozei@spyhce.com>
2021-03-21 23:56:18 +0200

    add providers

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,13 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { ChatService } from './shared/chat.service';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    MapComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+  ],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
