c88d81a67ff40c0c472cde4d7b3191c1b046e767
florin <florin.tomozei@spyhce.com>
2021-03-21 23:24:54 +0200

    add latlng to chat dto

--- apps/api/src/app/chat/dto/create-chat.dto.ts
+++ apps/api/src/app/chat/dto/create-chat.dto.ts
@@ -1,1 +1,9 @@
-export class CreateChatDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateChatDto {
+    @ApiProperty()
+    lat: number;
+
+    @ApiProperty()
+    lng: number;
+}
