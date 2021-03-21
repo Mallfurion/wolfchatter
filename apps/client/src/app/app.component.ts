a0a21df649602ab77d4c0023df257f6a430b0d3a
florin <florin.tomozei@spyhce.com>
2021-03-21 20:54:29 +0200

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wolfchatter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
